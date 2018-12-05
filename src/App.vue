c0de7cde9c2e3f9689cef33e46cc51866d7ea1c9
Radek <radek511@op.pl>
2018-12-05 14:09:48 +0000

    Add - Sale component with styles

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <div id="app">
     <Navbar />
     <router-view/>
+    <a class="el-button el-button--danger is-round is-plain" link round >Siema</a>
+    <Sale></Sale>
   </div>
 </template>
 
 <script>
+
 import Navbar from './components/Navbar/Navbar.vue';
+import Sale from './components/Sale/Sale.vue';
 
 export default {
   components:{
     Navbar,
+    Sale
   }
 }
 </script>
