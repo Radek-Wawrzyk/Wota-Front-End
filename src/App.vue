43ce82c155f0f36601db8ac6f1af1c3a40650b0b
Radek <radek511@op.pl>
2018-12-05 20:46:43 +0000

    Edit - About component styles

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
   <div id="app">
     <Navbar />
     <router-view/>
-    <About />
-    <Categories />
-    <p>Dupa jasia</p>
-    <Sale />
+    
     
     
     <p>All buttons to test</p>
@@ -18,16 +15,11 @@
 <script>
 
 import Navbar from './components/Navbar/Navbar.vue';
-import Sale from './components/Sale/Sale.vue';
-import Categories from './components/Categories/Categories.vue';
-import About from './components/About/About.vue';
+
 
 export default {
   components:{
-    Navbar,
-    Sale,
-    Categories,
-    About
+    Navbar
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <main class="home">
-
+    <About />
+    <Categories />
+    <Sale />
   </main>
 </template>
 
 <script>
+
+import Sale from '../components/Sale/Sale.vue';
+import Categories from '../components/Categories/Categories.vue';
+import About from '../components/About/About.vue';
+
 export default {
-  name: "home"
+  name: 'home',
+  components: {
+    Sale,
+    Categories,
+    About
+  }
 };
+
 </script>
