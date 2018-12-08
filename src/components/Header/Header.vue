a3558642e3f25e9beb410be0559d002c37f98c00
Hubert Kuzdak <Hubert@MacBook-Pro-Administrator.local>
2018-12-08 15:37:04 +0100

    navbar

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" src='./Header.scss'/>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,5 +1,21 @@
 <template>
-  <h3>Navbar</h3>
+  <nav class="navbar">
+    <div class="nav-logo">
+      <img src="../../assets/img/logo.png">
+    </div>
+    <div>
+      <ul class="nav-list">
+        <li>Główna</li>
+        <li>Kursy</li>
+        <li>Instruktorzy</li>
+        <li>Pojazdy</li>
+        <li>Projekty</li>
+        <li class>
+          <a href="#kontakt" class="btn btn-outline">Kontakt</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
