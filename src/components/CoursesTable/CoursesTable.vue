e7e3b54f8a1dec7ea4addcad628028c4450b85c6
Hubert Kuzdak <hubert.kuzdak@gmail.com>
2019-01-16 22:45:43 +0100

    add categories links

--- src/components/CoursesTable/CoursesTable.vue
+++ src/components/CoursesTable/CoursesTable.vue
@@ -12,7 +12,7 @@
       </tr>
     </thead>
     <tbody>
-      <router-link to="/kursy/dobry-ziomek" class="link">
+      <router-link to="/kursy/kategoria-A" class="link">
         <tr>
           <th>
             <img :src="require('../../assets/img/kategorie2/A.png')" alt="Kategoria A">
@@ -27,110 +27,126 @@
           </th>
         </tr>
       </router-link>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/A1.png')" alt="kategoria A1">
-        </th>
-        <th>A1</th>
-        <th>Praktyka</th>
-        <th>Teoria</th>
-        <th>Uwagi</th>
-        <th>Cena kursu</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/B.png')" alt="Kategoria B">
-        </th>
-        <th>B</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/B1.png')" alt="Kategoria B2">
-        </th>
-        <th>B2</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/BE.png')" alt="Kategoria BE">
-        </th>
-        <th>BE</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/C.png')" alt="kategoria C">
-        </th>
-        <th>C</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/D.png')" alt="kategoria D">
-        </th>
-        <th>D</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/T.png')" alt="kategoria T">
-        </th>
-        <th>T</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
-      <tr>
-        <th>
-          <img :src="require('../../assets/img/kategorie2/TAXI.png')" alt="kategoria taxi">
-        </th>
-        <th>TAXI</th>
-        <th>30h</th>
-        <th>25h</th>
-        <th>Brak</th>
-        <th>1 300zł</th>
-        <th class="table-cell-button">
-          <i class="el-icon-caret-right"></i>
-        </th>
-      </tr>
+      <router-link to="/kursy/kategoria-A1" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/A1.png')" alt="kategoria A1">
+          </th>
+          <th>A1</th>
+          <th>Praktyka</th>
+          <th>Teoria</th>
+          <th>Uwagi</th>
+          <th>Cena kursu</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-B" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/B.png')" alt="Kategoria B">
+          </th>
+          <th>B</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-B1" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/B1.png')" alt="Kategoria B2">
+          </th>
+          <th>B2</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-BE" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/BE.png')" alt="Kategoria BE">
+          </th>
+          <th>BE</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-C" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/C.png')" alt="kategoria C">
+          </th>
+          <th>C</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-D" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/D.png')" alt="kategoria D">
+          </th>
+          <th>D</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-T" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/T.png')" alt="kategoria T">
+          </th>
+          <th>T</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
+      <router-link to="/kursy/kategoria-TAXI" class="link">
+        <tr>
+          <th>
+            <img :src="require('../../assets/img/kategorie2/TAXI.png')" alt="kategoria taxi">
+          </th>
+          <th>TAXI</th>
+          <th>30h</th>
+          <th>25h</th>
+          <th>Brak</th>
+          <th>1 300zł</th>
+          <th class="table-cell-button">
+            <i class="el-icon-caret-right"></i>
+          </th>
+        </tr>
+      </router-link>
     </tbody>
   </table>
 </template>
