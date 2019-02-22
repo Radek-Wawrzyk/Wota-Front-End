6858ed7076f6741e23d7d83510263636b8d93ce5
Radek <radek511@op.pl>
2019-02-22 13:53:20 +0000

    Add - Ajax request for Course Page

--- src/views/CoursePage/CoursePage.vue
+++ src/views/CoursePage/CoursePage.vue
@@ -2,35 +2,35 @@
   <main>
     <SubHeader title="Kurs"></SubHeader>
     <div class="container">
-      <div class="course-page-header">
-        <img :src="require('../../assets/img/kategorie/A.png')" alt="Kategoria A">
-        <h1>A</h1>
+      <header class="course-page-header">
+        <img :src="course.icon" :alt="course.title">
+        <h1>{{course.title}}</h1>
         <h6>Kategoria</h6>
-        <p>Consectetur laborum irure sit elit mollit. Eu aliquip amet voluptate laboris deserunt aliqua. Amet id incididunt mollit ullamco veniam magna voluptate. Esse nulla aliquip duis ad irure incididunt id aute. Proident magna eiusmod sit aliquip elit adipisicing. Non minim anim eiusmod pariatur officia do est enim fugiat consectetur sit.</p>
-      </div>
-      <div>
+        <p>{{course.description}}</p>
+      </header>
+      <div class="course-page-content">
         <el-row>
-          <el-col :span="12">
+          <el-col :md="12">
             <el-row>
               <el-col :span="24" class="category-info-details p-r">
                 <div>Cena:</div>
-                <div>1350zł</div>
+                <div>{{course.parameters.price}}</div>
               </el-col>
               <el-col :span="24" class="category-info-details p-r">
                 <div>Teoria:</div>
-                <div>35h</div>
+                <div>{{course.parameters.teory}}</div>
               </el-col>
             </el-row>
           </el-col>
-          <el-col :span="12">
+          <el-col :md="12">
             <el-row>
               <el-col :span="24" class="category-info-details p-l">
                 <div>Praktyka:</div>
-                <div>25h</div>
+                <div>{{course.parameters.price}}</div>
               </el-col>
               <el-col :span="24" class="category-info-details p-l">
                 <div>Jazda dodatkowa:</div>
-                <div>55zł</div>
+                <div>{{course.parameters.additional}}</div>
               </el-col>
             </el-row>
           </el-col>
@@ -46,10 +46,13 @@
     </div>
   </main>
 </template>
+
 <script>
 import Sale from "../../components/Sale/Sale.vue";
 import CoursesTable from "../../components/CourseTable/CourseTable";
 import Gallery from "../../components/Gallery/Gallery";
+import axios from "axios";
+import { API } from '@/main.js';
 
 export default {
   name: "coursePage",
@@ -67,7 +70,7 @@ export default {
   async created() {
     try {
       const response = await axios.get(`${API}/courses/${this.id}`);
-      response.data ? this.course = response.data : false;
+      response.data ? (this.course = response.data.course) : false;
     } catch(error) {
       console.log(error.message);
     }
