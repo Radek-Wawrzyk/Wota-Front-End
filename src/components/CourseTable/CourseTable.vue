7c9d39fb7bd0efdd5a5ea2db4d554ae42285e0b5
Hubert Kuzdak <hubert.kuzdak@gmail.com>
2019-03-07 02:18:10 +0100

    fixes

--- src/components/CourseTable/CourseTable.vue
+++ src/components/CourseTable/CourseTable.vue
@@ -15,26 +15,38 @@
       <el-col :span="8" class="table-row">{{sch.theoryDays}}</el-col>
       <el-col :span="8" class="table-row">{{sch.hoursFromTo[0]}} - {{sch.hoursFromTo[1]}}</el-col>
     </el-row>
-    <el-row class="table-place-title" v-if="this.schedule.length >= 1">
-      <el-col :span="24">
-        <h4>Nowy Józefów 6</h4>
-      </el-col>
-    </el-row>
-    <el-row class="table-row-wrapper" v-for="(sch, index) in this.schedule[1].values" :key="index">
-      <el-col :span="8" class="table-row">{{sch.dayFromTo[0]}} - {{sch.dayFromTo[1]}}</el-col>
-      <el-col :span="8" class="table-row">{{sch.theoryDays}}</el-col>
-      <el-col :span="8" class="table-row">{{sch.hoursFromTo[0]}} - {{sch.hoursFromTo[1]}}</el-col>
-    </el-row>
-    <el-row class="table-place-title" v-if="this.schedule.length >= 1">
-      <el-col :span="24">
-        <h4>Retkińska 83</h4>
-      </el-col>
-    </el-row>
-    <el-row class="table-row-wrapper" v-for="(sch, index) in this.schedule[1].values" :key="index">
-      <el-col :span="8" class="table-row">{{sch.dayFromTo[0]}} - {{sch.dayFromTo[1]}}</el-col>
-      <el-col :span="8" class="table-row">{{sch.theoryDays}}</el-col>
-      <el-col :span="8" class="table-row">{{sch.hoursFromTo[0]}} - {{sch.hoursFromTo[1]}}</el-col>
-    </el-row>
+    <span v-if="this.schedule[1]">
+      <el-row class="table-place-title" v-if="this.schedule[1]">
+        <el-col :span="24">
+          <h4>Nowy Józefów 6</h4>
+        </el-col>
+      </el-row>
+      <el-row
+        class="table-row-wrapper"
+        v-for="(sch, index) in this.schedule[1].values"
+        :key="index"
+      >
+        <el-col :span="8" class="table-row">{{sch.dayFromTo[0]}} - {{sch.dayFromTo[1]}}</el-col>
+        <el-col :span="8" class="table-row">{{sch.theoryDays}}</el-col>
+        <el-col :span="8" class="table-row">{{sch.hoursFromTo[0]}} - {{sch.hoursFromTo[1]}}</el-col>
+      </el-row>
+    </span>
+    <span v-if="this.schedule[2]">
+      <el-row class="table-place-title">
+        <el-col :span="24">
+          <h4>Retkińska 83</h4>
+        </el-col>
+      </el-row>
+      <el-row
+        class="table-row-wrapper"
+        v-for="(sch, index) in this.schedule[2].values"
+        :key="index"
+      >
+        <el-col :span="8" class="table-row">{{sch.dayFromTo[0]}} - {{sch.dayFromTo[1]}}</el-col>
+        <el-col :span="8" class="table-row">{{sch.theoryDays}}</el-col>
+        <el-col :span="8" class="table-row">{{sch.hoursFromTo[0]}} - {{sch.hoursFromTo[1]}}</el-col>
+      </el-row>
+    </span>
   </div>
 </template>
 
