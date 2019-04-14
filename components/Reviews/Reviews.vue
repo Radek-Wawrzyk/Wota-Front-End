5bcbbf9cbb5a28db1250fcd25734a113eb777cc3
Radek <radek511@op.pl>
2019-04-15 01:16:02 +0200

    Reduced - Slider reviews opinion text

--- components/Reviews/Reviews.vue
+++ components/Reviews/Reviews.vue
@@ -18,7 +18,7 @@
           <!-- <figure class="testimonial-img">
             <img :src="testimonial.img" :alt="testimonial.author" />
           </figure>-->
-          <p class="testimonial-content">{{testimonial.comment}}</p>
+          <p class="testimonial-content">{{testimonial.comment | reduceText}}</p>
           <footer class="testimonial-author" v-if="testimonial.instructor">
             Opinia Instruktora:
             <span>{{testimonial.instructor}}</span>
@@ -36,34 +36,15 @@ import axios from "axios";
 export default {
   name: "Reviews",
   data: () => ({
-    testimonials: [
-      {
-        author: "Jan Kowalski",
-        content:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam voluptate earum et recusandae officia. Aut hic id ea, ipsam odio delectus vero? Iure tenetur alias fuga. Tempora, atque omnis.",
-        img:
-          "https://appworksinc.com/wp-content/uploads/2013/05/Team-Member-3.jpg"
-      },
-      {
-        author: "Jacek Bąk",
-        content:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam voluptate earum et recusandae officia. Aut hic id ea, ipsam odio delectus vero? Iure tenetur alias fuga. Tempora, atque omnis.",
-        img:
-          "https://appworksinc.com/wp-content/uploads/2013/05/Team-Member-3.jpg"
-      },
-      {
-        author: "Zygmunt Waza",
-        content:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam voluptate earum et recusandae officia. Aut hic id ea, ipsam odio delectus vero? Iure tenetur alias fuga. Tempora, atque omnis.",
-        img:
-          "https://appworksinc.com/wp-content/uploads/2013/05/Team-Member-3.jpg"
-      }
-    ],
-    prev:
-      '<svg aria-hidden="true" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
-    next:
-      '<svg aria-hidden="true" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>'
+    testimonials: [],
+    prev: '<svg aria-hidden="true" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
+    next: '<svg aria-hidden="true" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>'
   }),
+  filters: {
+    reduceText: text => {
+      return text.length >= 400 ? text.substring(0, 400) + ' ...' : text
+    }
+  },
   async created() {
     try {
       const response = await axios.get(`${this.$API}/rate`);
